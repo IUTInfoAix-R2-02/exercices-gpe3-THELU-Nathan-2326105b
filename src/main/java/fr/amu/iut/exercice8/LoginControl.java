1eb301c5e2e0b52cd967fa94a095967bc4278d08
Nathan Thélu <nathan.thelu@etu.univ-amu.fr>
2024-05-16 17:00:51 +0200

    Exercice 8 fini

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,38 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
-    private void okClicked() {
-    }
+    @FXML
+    private PasswordField Uid;
 
+    @FXML
+    private PasswordField pwd;
+
+    @FXML
+    private Button okid;
+
+    @FXML
+    private Button cancelid;
+
+
+    @FXML
+    private void okClicked() {
+        String mot = "";
+        for(int i = 0; i<pwd.getLength(); i++){
+            mot+='*';
+        }
+        System.out.println(Uid.getText());
+        System.out.println(mot);
+    }
+    @FXML
     private void cancelClicked() {
+        Uid.setText("");
+        pwd.setText("");
+
     }
 }
